3823afb4dfc67279f5c37ce99a990cf87d34521d
Mad-Mex <pablodigital85@gmail.com>
2022-12-28 11:54:09 -0600

    "Se modifica newsletter"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage/HomePage';
-import './App.css';
-import { FiguresPage } from './pages/FiguresPage/FiguresPage';
-import { PaintingsPage } from './pages/PaintingsPage/PaintingsPage';
-import { ProductPage } from './pages/ProductPage/ProductPage';
-import './App.css';
-import { ScrollToTop } from './components/Navigation/ScrollToTop/ScrollToTop';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { FiguresPage } from "./pages/FiguresPage/FiguresPage";
+import { PaintingsPage } from "./pages/PaintingsPage/PaintingsPage";
+import { ProductPage } from "./pages/ProductPage/ProductPage";
+import { ScrollToTop } from "./components/Navigation/ScrollToTop/ScrollToTop";
+import { Footer } from "./components/Footer/Footer";
+import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
-
   return (
-
     <BrowserRouter>
+      <ScrollToTop />
+      <Header />
       <Routes>
-        <Route path='/' element={ <HomePage /> } />
-        <Route path='/categoria/figuras' element={ <FiguresPage /> } />
-        <Route path='/categoria/cuadros' element={ <PaintingsPage /> } />
-        <Route path='/detalle/figuras' element={ <ProductPage /> }  />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/categoria/figuras" element={<FiguresPage />} />
+        <Route path="/categoria/cuadros" element={<PaintingsPage />} />
+        <Route path="/detalle/figuras" element={<ProductPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-    
   );
 }
 
--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,3 +1,5 @@
+import "./Newsletter.css"
+
 export const Newsletter = () => {
 
   return (
