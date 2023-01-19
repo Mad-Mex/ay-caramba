00dfd4478a88deecf117eca1f4057fb22c6b57aa
Elías De Fabrittis <defa@outlook.com.ar>
2023-01-19 17:53:54 -0300

    agrego detalle a card

--- src/components/Figure/FigureDetail/FigureDetail.js
+++ src/components/Figure/FigureDetail/FigureDetail.js
@@ -9,7 +9,7 @@ export const FigureDetail = ({ figure }) => {
         <img src={ figure.image } alt={ figure.name } className="w-[24rem] lg:w-[16rem] h-[25rem] lg:h-[17rem] rounded-2xl"   />
      </div> 
 
-     <div>
+     <div className="text-center lg:text-left">
         <h2 className='mb-7 text-4xl text-tertiary'> { `${figure.name} / ${ figure.size } ` } </h2>
         <h3 className='text-lg' > Materiales </h3>
         <p className='text-sm'> { `-${figure.material_1}` } </p>
